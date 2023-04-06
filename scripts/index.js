77483dfc9ce2d6c1453d47a577d75d244f3f280d
cjrodriguez12 <120134645+cjrodriguez12@users.noreply.github.com>
2023-04-06 19:38:14 -0400

    stage 3
    work in progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,83 @@
-let initialCards=[
-    {
-    name:"Yosemite Valley",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
 
-},
-{
-    name:"Lake Louise",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-
-},
-
- {
-    name:"Bald Mountains",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-
-},
-{
-    name:"Latemar",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-
-},
-{
-    name:"Vanoise National Park",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-
-},
-{
-    name:"Lago di Braies",
-    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg "
-
-}
-]
-
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
+  },
+];
+/** elements**/
 const profileEditButton = document.querySelector(".profile__edit");
 const profileClose = document.querySelector(".modal__container-close");
-const modalOpened = document.querySelector(".modal")
-function openModal(){
-    modalOpened.classList.add("modal_opened");
+const modalOpened = document.querySelector(".modal");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector('.gallery__card');
+/**functions */
+function openModal() {
+  modalOpened.classList.add("modal_opened");
+  profileTitleInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 }
-function closeModal(){
-    modalOpened.classList.remove("modal_opened");
+function closeModal() {
+  modalOpened.classList.remove("modal_opened");
 }
+function getCardElement(cardData) {
+  //clone template
+  const cardElement = cardTemplate.cloneNode(true);
+  //access title and image
+  const cardImageEl = cardElement.querySelector('.gallery__card-image');
+  const cardTitleEl = cardElement.querySelector('.galery__card-title');
+  //set path to image
+  //cardImageEl.src
+  //set alt text to name
+  //set title to card name
+  cardTitleEl.textContent = cardData.name;
+  //return ready html
+  return cardElement;
+}
+/**Event Handler */
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
+}
+/**Event Listener */
 profileEditButton.addEventListener("click", openModal);
-profileClose.addEventListener("click",closeModal);
+profileClose.addEventListener("click", closeModal);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+  
+});
+  
